c86df0f53ca17b066e2f83f65aced17c7e5f1b55
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-03-26 16:21:50 -0600

    Add single-user data file and started writing User class

---
+++ data/single-user.js
@@ -0,0 +1,19 @@
+let usersData = [
+  {
+    "name": "Saige O'Kon",
+    "id": 1,
+    "pantry": [
+      {
+        "ingredient": 11477,
+        "amount": 4
+      },
+      {
+        "ingredient": 11297,
+        "amount": 4
+      }
+    ]
+  }];
+
+  module.exports = {
+    usersData
+  }
