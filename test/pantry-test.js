affabd047061e67a61cceec747cd462280e50355
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-03-29 19:55:34 -0600

    Add 3 methods in the Pantry class and 1 in the Recipe class

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -7,32 +7,60 @@ const recipeData = require('../data/recipes');
 const userData = require('../data/users');
 
 describe('Pantry', function() {
-    let recipe1;
-    let pantry1;
+  let recipe1;
+  let pantry1;
 
 
-    beforeEach(() => {
-      recipe1 = new Recipe(recipeData[0].id, recipeData[0].image, recipeData[0].ingredients, recipeData[0].instructions, recipeData[0].name, recipeData[0].tags);
-      pantry1 = new Pantry(userData[0].pantry);
+  beforeEach(() => {
+    recipe1 = new Recipe(recipeData[0].id, recipeData[0].image, recipeData[0].ingredients, recipeData[0].instructions, recipeData[0].name, recipeData[0].tags);
+    pantry1 = new Pantry(userData[0].pantry);
 
-    });
+  });
 
-    it('should be a function', function() {
+  it('should be a function', function() {
 
-        expect(Pantry).to.be.a('function');
-        });
+    expect(Pantry).to.be.a('function');
+  });
 
-    it('should be an instance of Pantry', function() {
+  it('should be an instance of Pantry', function() {
 
-        // expect(user).to.be.an.instanceof(Pantry);
-        });
+    expect(pantry1).to.be.an.instanceof(Pantry);
+  });
 
-    it.only('should be able to determine if there is enough ingredients for a recipe', function() {
+  it('should get the pantry ingredients ids and amounts', function() {
 
-        pantry1.checkPantryForIngredients(recipe1)
-        expect(pantry1.checkPantryForIngredients(recipe1)).to.eq(false);
+    pantry1.getPantryIdsAndAmounts();
+    expect(pantry1.getPantryIdsAndAmounts().length).to.eq(36);
+  });
 
-    });
+  it('should find the missing ingredients in the pantry compared to the recipe', function() {
 
-    
+    pantry1.findIngredientsMissing(recipe1);
+    expect(pantry1.findIngredientsMissing(recipe1)).to.deep.eq([
+      { id: 20081, amount: 5 },
+      { id: 18372, amount: 3 },
+      { id: 1123, amount: 8 },
+      { id: 19335, amount: 4 },
+      { id: 19206, amount: 2 },
+      undefined,
+      { id: 2047, amount: 6 },
+      undefined,
+      undefined,
+      { id: 1145, amount: 4 },
+      { id: 2050, amount: 4 }
+    ])
+  });
+
+it('should return the missing ingredients needed and their amounts to cook the recipe', function() {
+
+  pantry1.findIngredientsNeeded(recipe1);
+  expect(pantry1.findIngredientsNeeded(recipe1)).to.deep.eq([
+      { id: 19206, amount: 1 },
+      { id: 19334, amount: 0.5 },
+      { id: 1012047, amount: 24 },
+      { id: 10019903, amount: 2 }
+    ])
+});
+
+
 });
--- test/recipe-test.js
+++ test/recipe-test.js
@@ -46,13 +46,31 @@ describe('Recipe', function() {
 
     it('should calculate the cost of all ingredients', function() {
         recipe1.getCostOfIngredients();
-        expect(recipe1.getCostOfIngredients()).to.equal(17776)
+        expect(recipe1.getCostOfIngredients()).to.eq(17776)
     });
 
     it('should return instructions', function() {
       recipe1.getInstructions();
-      expect(recipe1.instructions).to.deep.equal(recipeData[0].instructions)
+      expect(recipe1.instructions).to.deep.eq(recipeData[0].instructions)
     });
 
-    
+    it('should get the ids and the amounts of a recipe', function() {
+
+      recipe1.getRecipeIdsAndAmounts();
+      expect(recipe1.getRecipeIdsAndAmounts()).to.deep.eq([
+        { id: 20081, amount: 1.5 },
+        { id: 18372, amount: 0.5 },
+        { id: 1123, amount: 1 },
+        { id: 19335, amount: 0.5 },
+        { id: 19206, amount: 3 },
+        { id: 19334, amount: 0.5 },
+        { id: 2047, amount: 0.5 },
+        { id: 1012047, amount: 24 },
+        { id: 10019903, amount: 2 },
+        { id: 1145, amount: 0.5 },
+        { id: 2050, amount: 0.5 }
+      ])
+    });
+
+
 });
