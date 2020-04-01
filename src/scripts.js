a4b6954ab00c9863e4f04fd2cf5e5ed7b04a496e
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-04-01 15:46:32 -0600

    scripts add to fav array

--- src/scripts.js
+++ src/scripts.js
@@ -21,32 +21,66 @@ const displayRecipes = () => {
         <img class="recipe-image" src="${recipe.image}" />
       </section>
       <section class="recipe-buttons">
-        <button class="recipe-button fav"><img src="assets/unfavorited.svg" alt="unfilled in heart"></button>
-        <button class="recipe-button cook"><img src="assets/not_added_to_cook.svg" alt="unfilled in silverware"></button>
-      </section>
-    </div>`
-
-
-  })
-}
-
+        <button data-id="${recipe.id}" "class="recipe-button fav"><img data-id="${recipe.id}" class="empty-heart filled-heart" src="assets/unfavorited.svg" alt="unfilled in heart"></button>
+        <button class="recipe-button cook"><img data-id="${recipe.id}" class="toCook" src="assets/not_added_to_cook.svg" alt="unfilled in silverware"></button>
+        </section>
+        </div>`    
+      })
+     
+    }
+    
 const load = () => {
   loadUser();
   displayRecipes();
 }
 
-const addToFavorites = (e) => {
-  console.log(e.target)
+// const addToFavorites = (e) => {
+//   console.log(e.target)
   // if (e.target.closest('.recipe-button'))
   // e.target === recipe-button
   //closest section with buttons (traverse up and then back down). closest recipe buttons class and then back down
-}
+  // }
 
 recipesContainer.addEventListener('click', addToFavorites)
+  
+function addToFavorites(event) {
+  let recipeCard = event.target.dataset.id
+  const currentRecipe = recipeData.find(recipe => recipe.id == recipeCard)
+  console.log('before', user.favoriteRecipes)
+  user.toggleFavoriteRecipe(currentRecipe)
+  console.log('after', user.favoriteRecipes)
+  addUnfavoritedIcon(recipeCard)
+  recipeData.forEach(recipe => {
+    user.favoriteRecipes.forEach(favRecipe => {
+      if(recipe.id = favRecipe.id) {
+        addFavedIcon(recipeCard)
+      }
+      
+    });
 
+  })
+}
 
+function addFavedIcon(recipeCard) {
+  const photo = document.querySelector(`.empty-heart[data-id="${recipeCard}"]`)
+  photo.src="assets/favorited.svg"
+}  
 
+function addUnfavoritedIcon(recipeCard) {
+  const photo = document.querySelector(`.filled-heart[data-id="${recipeCard}"]`)
+  photo.src="assets/unfavorited.svg"
+}
+// recipesContainer.addEventListener('click', removeFromFavorites)
 
+// function removeFromFavorites(event) {
+//   let recipeCard = event.target.dataset.id
+//   const currentRecipe = recipeData.find(recipe => recipe.id == recipeCard)
+//   user.toggleFavoriteRecipe(currentRecipe)
+//   addUnfavoritedIcon(recipeCard)
+//   // addFavedIcon(recipeCard)
+//   // if(user.favoriteRecipes.includes(recipeCard)) {
+//   // } 
+// }
 
 
 window.onload = load();
